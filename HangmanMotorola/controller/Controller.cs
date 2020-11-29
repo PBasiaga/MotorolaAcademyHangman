73261f3daa842e907f7e07834b01345210863c51
Przemysław Basiaga <przemek.basiaga23@gmail.com>
2020-11-29 20:39:16 +0100

    create instances of View, Player and Game in Controller, implement initialization of new game

--- HangmanMotorola/controller/Controller.cs
+++ HangmanMotorola/controller/Controller.cs
@@ -1,10 +1,32 @@
-﻿namespace HangmanMotorola.controller
+﻿using System.Collections.Generic;
+using HangmanMotorola.model.data;
+using HangmanMotorola.view;
+
+namespace HangmanMotorola.controller
 {
     public class Controller
     {
+        private View view;
+        private Player player;
+        private Game game;
+
+
+        public Controller()
+        {
+            view = new View();
+        }
+        
         public void Start()
         {
-            
+        
+        
+        
+        }
+        
+        private void InitializeGame()
+        {
+            player = new Player();
+            game = new Game();
         }
     }
 }
