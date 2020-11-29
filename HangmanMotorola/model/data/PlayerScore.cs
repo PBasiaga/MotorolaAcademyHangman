9f4adcc466548cf5ffb9f36a1ea9a980a18b2714
Przemysław Basiaga <przemek.basiaga23@gmail.com>
2020-11-29 22:44:02 +0100

    implement class PlayerScore

---
+++ HangmanMotorola/model/data/PlayerScore.cs
@@ -0,0 +1,53 @@
+﻿namespace HangmanMotorola.model.data
+{
+    public class PlayerScore
+    {
+        private string name;
+        
+        private string lifePoints;
+
+        private string guessingTime;
+
+        private string guessingTries;
+
+        private string guessedPassword;
+
+        private string dateOfPlay;
+        
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string LifePoints
+        {
+            get;
+            set;
+        }
+
+        public string GuessingTime
+        {
+            get;
+            set;
+        }
+
+        public string GuessingTries
+        {
+            get;
+            set;
+        }
+
+        public string GuessedPassword
+        {
+            get;
+            set;
+        }
+
+        public string DateOfPlay
+        {
+            get;
+            set;
+        }
+    }
+}
