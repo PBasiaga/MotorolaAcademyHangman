7b20127275a8997d6d8c410978a8b7d5937c8934
Przemysław Basiaga <przemek.basiaga23@gmail.com>
2020-11-29 20:52:56 +0100

    implement func showing starting screen with instructions

--- HangmanMotorola/view/View.cs
+++ HangmanMotorola/view/View.cs
@@ -4,6 +4,21 @@ namespace HangmanMotorola.view
 {
     public class View
     {
+        
+        public void ShowStartingScreen()
+        {
+            Console.WriteLine("\nWelcome to the Hangman game! \n" +
+                              "Guess hidden word to win the game! \n" +
+                              "Password is chosen randomly, but it's always a capital of a country. \n" +
+                              "Before guessing, choose whether you want to guess a letter or a whole word. \n" +
+                              "Be careful, you have 5 lifepoints, guessing wrong will cost you some of them! \n" +
+                              "Guess quickly and you might join the leaderboard! Good Luck! \n \n" +
+                              "Press any key to continue.");
+            System.Console.ReadKey();
+            Console.Clear();
+            
+        }
+        
         public void ShowBoard(char[] board)
         {
             Console.WriteLine("\n");
