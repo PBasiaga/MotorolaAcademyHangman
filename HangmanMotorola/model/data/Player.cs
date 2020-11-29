3bbd2f72c4f264777ef4d06182842467cbe5d1a2
Przemysław Basiaga <przemek.basiaga23@gmail.com>
2020-11-29 20:22:45 +0100

    implement Player class

--- HangmanMotorola/model/data/Player.cs
+++ HangmanMotorola/model/data/Player.cs
@@ -2,6 +2,45 @@
 {
     public class Player
     {
+        private string name;
         
+        private int lifePoints;
+
+        private int guessingTime;
+
+        private int guessingTries;
+
+        public Player()
+        {
+            LifePoints = 5;
+            GuessingTime = 0;
+            GuessingTries = 0;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public int LifePoints
+        {
+            get; set;
+        }
+
+        public int GuessingTime
+        {
+            get; set;
+        }
+
+        public int GuessingTries
+        {
+            get; set;
+        }
+        
+
+        public string DateOfPlay
+        {
+            get; set;
+        }
     }
 }
