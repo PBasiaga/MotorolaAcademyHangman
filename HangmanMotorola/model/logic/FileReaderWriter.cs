524662c1f876a5f2f943a99e430ce752eec2a611
Przemysław Basiaga <przemek.basiaga23@gmail.com>
2020-11-29 21:55:52 +0100

    create path to hangman_art.txt in FileReaderWriter

--- HangmanMotorola/model/logic/FileReaderWriter.cs
+++ HangmanMotorola/model/logic/FileReaderWriter.cs
@@ -9,6 +9,9 @@ namespace HangmanMotorola.model.logic
         private const string passwordsAndHintsFile =
             "D:/Programowanie/backEnd/C# - programowanie/MotorolaAcademyTask/HangmanMotorola/HangmanMotorola/resources/countries_and_capitals.txt";
         
+        private const string hangmanArtFile = 
+            "D:/Programowanie/backEnd/C# - programowanie/MotorolaAcademyTask/HangmanMotorola/HangmanMotorola/resources/hangman_art.txt";
+        
         private Random random = new Random();
         
         public void GetRandomPasswordAndHint(out string password, out string hint)
