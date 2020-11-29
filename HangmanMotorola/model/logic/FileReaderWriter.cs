0d876c8183f368b4c793cf05a485f34c648306d7
Przemysław Basiaga <przemek.basiaga23@gmail.com>
2020-11-29 20:18:03 +0100

    implement GetRandomPasswordAndHint function in FileReaderWriter.cs

--- HangmanMotorola/model/logic/FileReaderWriter.cs
+++ HangmanMotorola/model/logic/FileReaderWriter.cs
@@ -1,7 +1,28 @@
-﻿namespace HangmanMotorola.model.logic
+﻿using System;
+using System.IO;
+
+namespace HangmanMotorola.model.logic
 {
     public class FileReaderWriter
     {
         
+        private const string passwordsAndHintsFile =
+            "D:/Programowanie/backEnd/C# - programowanie/MotorolaAcademyTask/HangmanMotorola/HangmanMotorola/resources/countries_and_capitals.txt";
+        
+        private Random random = new Random();
+        
+        public void GetRandomPasswordAndHint(out string password, out string hint)
+        {
+            string[] lines = File.ReadAllLines(passwordsAndHintsFile);
+          
+            int randomNumber = random.Next(0, lines.Length - 1);
+
+            string randomLine = lines[randomNumber];
+
+            string[] passwordAndHint = randomLine.Split('|');
+
+            password = passwordAndHint[1].Trim().ToUpper();
+            hint = passwordAndHint[0].Trim().ToUpper();
+        }
     }
 }
