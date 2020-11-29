e05e29a0496d146c1df9c2312eb6835c19e4a208
Przemysław Basiaga <przemek.basiaga23@gmail.com>
2020-11-29 21:22:50 +0100

    implement check answer, checkFullWordAnswer and CheckOneLetterNaswer functions

--- HangmanMotorola/controller/Controller.cs
+++ HangmanMotorola/controller/Controller.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using HangmanMotorola.model.data;
 using HangmanMotorola.view;
 
@@ -32,7 +32,7 @@ namespace HangmanMotorola.controller
                 view.ShowRemainingLifePoints(player.LifePoints);
                 game.Answer = view.AskForInput().ToUpper();
                 player.GuessingTries++;
-                //check answer
+                CheckAnswer();
                 //check if gameover
             }
             //check outcome
@@ -43,6 +43,64 @@ namespace HangmanMotorola.controller
         {
             player = new Player();
             game = new Game();
+        }
+        
+        private void CheckAnswer()
+        {
+            if (game.Answer.Length == 0)
+            {
+                view.ShowEmptyAnswerInfo();
+            }
+            else if (game.Answer.Length == 1)
+            {
+                CheckOneLetterAnswer();
+            }
+            else
+            {
+                CheckFullWordAnswer();
+            }
+        }
+        
+        private void CheckFullWordAnswer()
+        {
+            if (game.Answer.Equals(game.Password))
+            {
+                game.IsGameFinished = true;
+            }
+            else
+            {
+                player.LifePoints -= 2;
+                //view.ShowWrongPasswordInfo();
+            }
+        }
+        
+        private void CheckOneLetterAnswer()
+        {
+            var character = game.Answer.ToUpper()[0];
+            
+            if (char.IsLetter(character))
+            {
+                if (game.GuessedLetters.Contains(character) || game.LettersNotInWord.Contains(character))
+                {
+                    view.ShowAlreadyGuessedInfo();
+                }
+                else if (game.Password.Contains(character))
+                {
+                    game.PutLettersInBoard(character);
+                    game.GuessedLetters.Add(character);
+                    view.ShowCorrectLetterInfo();
+                }
+                else
+                {
+                    game.LettersNotInWord.Add(character);
+                    player.LifePoints -= 1;
+                    view.ShowWrongLetterInfo();
+                }
+            }
+            else
+            {
+                view.ShowMustBeALetterInfo();
+            }
         }
     }
 }
